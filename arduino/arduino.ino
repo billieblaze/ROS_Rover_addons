7b65fdf7e3190794717a3a1985c2b25f3b121478
bblaze <bill@diffuseaudio.com>
2017-06-11 10:16:32 -0400

    rando commit

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -36,7 +36,7 @@ RunningMedian samples_3 = RunningMedian(10);
 unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
 unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
 uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
-int distanceLoopTime = 5000;
+int distanceLoopTime = 1000;
 int lastDistance = 0;
 
 
