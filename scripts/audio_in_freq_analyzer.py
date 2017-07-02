f41757ff5ac2ab7e5762215642af744bb3ee01c7
bill berzinskas <bberzinskas@bills-mbp.attlocal.net>
2017-07-01 21:41:06 -0400

    cleanup python stuff a little

--- scripts/audio_in_freq_analyzer.py
+++ scripts/audio_in_freq_analyzer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import rospy
-import json 
+import json
 import time
 
-import fft
-from sw_hear import SWHear
+import model.fft
+from model.sw_hear import SWHear
 import numpy as np
 
 from std_msgs.msg import String, Bool, Int16
 from sensor_msgs.msg import NavSatFix
-   
+
 current_location = NavSatFix()
 
 pub_details = rospy.Publisher('/sensor/audio', String, queue_size=10)
@@ -32,7 +32,7 @@ def listener():
     t1=0
     plot_sec = .25
     freq = 0
-    last_freq = 0 
+    last_freq = 0
 
     while True:
         ear.tape_add()
@@ -40,7 +40,7 @@ def listener():
             t1=time.time()
             #ear.tape_plot()
             print(ear.get_tape())
-            
+
             tape = ear.get_tape()
             rate = 44100
             samples = np.arange(len(tape))/rate,tape
@@ -49,12 +49,12 @@ def listener():
             freq = fft.freq_from_fft(np.nan_to_num(tape), rate)
             print '%f Hz'   %  freq
 
-            if freq != last_freq: 
+            if freq != last_freq:
                 pub_details.publish(json.dumps({"location": {"latitude": current_location.latitude, "longitude": current_location.longitude, "altitude": current_location.altitude},  "sensor": freq}))
                 last_freq = freq
 
-    
-    # ear.tape_forever()    
+
+    # ear.tape_forever()
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
     ear.close()
