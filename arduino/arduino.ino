aa6595ec91e53f06ca08b7c3c49b7a756e0952c4
Bill Berzinskas <bberzinskas@gmail.com>
2017-11-01 20:27:44 -0400

    ..

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -170,19 +170,14 @@ int convert_angle( int stepsPerRotation, double radians){
   return y*49;
 }
 
-void moveStepper( AccelStepper stepper, int stepsPerRotation, int radians){
+void moveStepper( AccelStepper stepper, int stepsPerRotation, int radians, int pointer){
   int distance = convert_angle(stepsPerRotation, radians);
   stepper.moveTo( distance );
+  prev_state[pointer] = joint_state[pointer];
 }
 
 void jointCallback( const sensor_msgs::JointState& cmd_msg ){
   nh.loginfo("callback");
-  prev_state[0] = joint_state[0];
-  prev_state[1] = joint_state[1];
-  prev_state[2] = joint_state[2];
-  prev_state[3] = joint_state[3];
-  prev_state[4] = joint_state[4];
-
   joint_state[0] = cmd_msg.position[0];
   joint_state[1] = cmd_msg.position[1];
   joint_state[2] = cmd_msg.position[2];
@@ -193,7 +188,7 @@ void jointCallback( const sensor_msgs::JointState& cmd_msg ){
 ros::Subscriber<sensor_msgs::JointState> sub("/move_group/fake_controller_joint_states", jointCallback );
 
 void setup(){
-  setupSteppers();
+
 
   nh.getHardware()->setBaud(115200);
   nh.initNode();
@@ -201,6 +196,7 @@ void setup(){
   while (!nh.connected()){
     nh.spinOnce();
   }
+    setupSteppers();
   //setupDistance();
 }
 
@@ -210,23 +206,23 @@ void loop()
   // nh.loginfo("loop");
   if (joint_state[0] != prev_state[0]){
     nh.loginfo("J1 state changed");
-    moveStepper(stepper1, 400, joint_state[0]);
+    moveStepper(stepper1, 400, 0);
   }
   if (joint_state[1] != prev_state[1]){
     nh.loginfo("J2 state changed");
-    moveStepper(stepper2, 400, joint_state[1]);
+    moveStepper(stepper2, 400, 1);
   }
   if (joint_state[2] != prev_state[2]){
     nh.loginfo("J3 state changed");
-    moveStepper(stepper3, 400, joint_state[2]);
+    moveStepper(stepper3, 400, 2);
   }
   if (joint_state[3] != prev_state[3]){
     nh.loginfo("J4 state changed");
-    moveStepper(stepper4, 400, joint_state[3]);
+    moveStepper(stepper4, 400, 3);
   }
   if (joint_state[4] != prev_state[4]){
     nh.loginfo("J5 state changed");
-    moveStepper(stepper5, 400, joint_state[4]);
+    moveStepper(stepper5, 400, 4);
   }
   //handleDistance();
   stepper1.run();
