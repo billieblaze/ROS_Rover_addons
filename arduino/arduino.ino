0553c4c1d51cc60749c533e5e3b7141e4f393021
Bill Berzinskas <bberzinskas@gmail.com>
2017-11-01 21:21:22 -0400

    ..

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -184,13 +184,13 @@ int convert_angle( int stepsPerRotation, double radians){
 void moveSteppers(){
   if (prev_state[0] != joint_state[0] || prev_state[1] != joint_state[1] || prev_state[2] != joint_state[2] || prev_state[3] != joint_state[3] || prev_state[4] != joint_state[4]){
     steppers.moveTo(joint_state);
-    //steppers.runSpeedToPosition();
     prev_state[0]=joint_state[0];
     prev_state[1]=joint_state[1];
     prev_state[2]=joint_state[2];
     prev_state[3]=joint_state[3];
     prev_state[4]=joint_state[4];
   }
+  steppers.runSpeedToPosition();
 }
 
 void jointCallback( const sensor_msgs::JointState& cmd_msg ){
