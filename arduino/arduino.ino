4867a21a285f96d8a6cadc0f867ef4d2ce554d3f
Bill Berzinskas <bberzinskas@gmail.com>
2017-11-01 23:00:24 -0400

    ..

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -188,7 +188,7 @@ void jointCallback( const sensor_msgs::JointState& cmd_msg ){
   // joint_state[1] = convert_angle(633, cmd_msg.position[1]);
   // joint_state[2] = -convert_angle(171, cmd_msg.position[2]);
   // joint_state[3] = convert_angle(200, cmd_msg.position[3]);
-  joint_state[4] = convert_angle(880, cmd_msg.position[4]);
+  joint_state[4] = convert_angle(281, cmd_msg.position[4]);
 }
 
 ros::Subscriber<sensor_msgs::JointState> sub("/move_group/fake_controller_joint_states", jointCallback );
