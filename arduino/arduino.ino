afa0dab05a07a5f8251ec3fa3903cb010ce2d89e
Bill Berzinskas <bberzinskas@gmail.com>
2017-11-01 21:36:25 -0400

    ..

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -183,23 +183,23 @@ int convert_angle( int stepsPerRotation, double radians){
 
 void moveSteppers(){
   if (prev_state[0] != joint_state[0] || prev_state[1] != joint_state[1] || prev_state[2] != joint_state[2] || prev_state[3] != joint_state[3] || prev_state[4] != joint_state[4]){
-    steppers.moveTo(joint_state);
+    steppers.move(joint_state);
     prev_state[0]=joint_state[0];
     prev_state[1]=joint_state[1];
     prev_state[2]=joint_state[2];
     prev_state[3]=joint_state[3];
     prev_state[4]=joint_state[4];
   }
-  steppers.runSpeedToPosition();
+  steppers.run();
 }
 
 void jointCallback( const sensor_msgs::JointState& cmd_msg ){
   // nh.loginfo("callback");
-  joint_state[0] = convert_angle(400, cmd_msg.position[0]);
-  joint_state[1] = convert_angle(400, cmd_msg.position[1]);
-  joint_state[2] = convert_angle(400, cmd_msg.position[2]);
-  joint_state[3] = convert_angle(400, cmd_msg.position[3]);
-  joint_state[4] = convert_angle(400, cmd_msg.position[4]);
+  joint_state[0] = convert_angle(400, cmd_msg.position[0]) - prev_state[0];
+  joint_state[1] = convert_angle(400, cmd_msg.position[1]) - prev_state[1];
+  joint_state[2] = convert_angle(400, cmd_msg.position[2]) - prev_state[2];
+  joint_state[3] = convert_angle(400, cmd_msg.position[3]) - prev_state[3];
+  joint_state[4] = convert_angle(400, cmd_msg.position[4]) - prev_state[4];
 }
 
 ros::Subscriber<sensor_msgs::JointState> sub("/move_group/fake_controller_joint_states", jointCallback );
