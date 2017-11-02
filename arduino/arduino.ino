88af9f4c6a9fca96ca017bfe093f70c7eefba3da
Bill Berzinskas <bberzinskas@gmail.com>
2017-11-01 21:05:08 -0400

    ..

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -185,7 +185,11 @@ void moveSteppers(){
   if (prev_state[pointer] != joint_state[pointer]){
     steppers.moveTo(joint_state);
     steppers.runSpeedToPosition();
-    prev_state=joint_state;
+    prev_state[0]=joint_state[0];
+    prev_state[1]=joint_state[1];
+    prev_state[2]=joint_state[2];
+    prev_state[3]=joint_state[3];
+    prev_state[4]=joint_state[4];
   }
 }
 
