b5aa37305e60a1773ed1b94a9dca786773fab986
Bill Berzinskas <bberzinskas@gmail.com>
2017-11-01 20:29:59 -0400

    ..

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -170,10 +170,13 @@ int convert_angle( int stepsPerRotation, double radians){
   return y*49;
 }
 
-void moveStepper( AccelStepper stepper, int stepsPerRotation, int radians, int pointer){
-  int distance = convert_angle(stepsPerRotation, radians);
-  stepper.moveTo( distance );
-  prev_state[pointer] = joint_state[pointer];
+void moveStepper( AccelStepper stepper, int stepsPerRotation, int pointer){
+  if (joint_state[pointer] != prev_state[pointer]){
+    int distance = convert_angle(stepsPerRotation, joint_state[pointer]);
+    stepper.moveTo( distance );
+    prev_state[pointer] = joint_state[pointer];
+  }
+  stepper.run();
 }
 
 void jointCallback( const sensor_msgs::JointState& cmd_msg ){
@@ -188,48 +191,27 @@ void jointCallback( const sensor_msgs::JointState& cmd_msg ){
 ros::Subscriber<sensor_msgs::JointState> sub("/move_group/fake_controller_joint_states", jointCallback );
 
 void setup(){
-
-
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(sub);
   while (!nh.connected()){
     nh.spinOnce();
   }
-    setupSteppers();
+
+  setupSteppers();
+
   //setupDistance();
 }
 
 
 void loop()
 {
-  // nh.loginfo("loop");
-  if (joint_state[0] != prev_state[0]){
-    nh.loginfo("J1 state changed");
     moveStepper(stepper1, 400, 0);
-  }
-  if (joint_state[1] != prev_state[1]){
-    nh.loginfo("J2 state changed");
     moveStepper(stepper2, 400, 1);
-  }
-  if (joint_state[2] != prev_state[2]){
-    nh.loginfo("J3 state changed");
     moveStepper(stepper3, 400, 2);
-  }
-  if (joint_state[3] != prev_state[3]){
-    nh.loginfo("J4 state changed");
     moveStepper(stepper4, 400, 3);
-  }
-  if (joint_state[4] != prev_state[4]){
-    nh.loginfo("J5 state changed");
     moveStepper(stepper5, 400, 4);
-  }
-  //handleDistance();
-  stepper1.run();
-  stepper2.run();
-  stepper3.run();
-  stepper4.run();
-  stepper5.run();
-  nh.spinOnce();
+
+    nh.spinOnce();
 
 }
